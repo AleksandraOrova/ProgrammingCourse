e11a32b4746706c198894b9644a2a72ca60f13d0
Aleksandra <alexorova310198@icloud.com>
2015-11-23 01:01:53 +0300

    Проект argc argv

--- sources/C_Project/main.c
+++ sources/C_Project/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    int i;
+    if(argc!=0){
+        for(i = 0; i<argc; i++)
+            printf("%s", *argv[i]);
+    }
     printf("Hello World!\n");
     return 0;
 }
