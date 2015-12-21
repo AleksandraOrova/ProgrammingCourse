0ab810d8281a9237f805d1cd94ab04fc5c021cdf
Aleksandra <alexorova310198@icloud.com>
2015-12-22 02:27:08 +0300

    Зачатки модульных тестов для СРР

--- sources/SubDirrrProject/testCPP/tst_testcpptest.cpp
+++ sources/SubDirrrProject/testCPP/tst_testcpptest.cpp
@@ -10,14 +10,19 @@ public:
 
 private Q_SLOTS:
     void testCase1();
+  // void bank_test();
+  // void home_test_1();
+  // void home_test_2();
+  // void cm2inch_test();
+  // void matrix_test();
+  // void strings_test();
 };
 
-TestCPPTest::TestCPPTest()
-{
+TestCPPTest::TestCPPTest(){
+
 }
 
-void TestCPPTest::testCase1()
-{
+void TestCPPTest::testCase1(){
     QVERIFY2(true, "Failure");
 }
 
